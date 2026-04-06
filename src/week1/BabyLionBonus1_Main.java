6698e18cea3b3a877abaa0aee6e736978059b694
장선호 <tmmjang@naver.com>
2026-04-06 15:37:59 +0900

    프로그램 반복 실행

---
+++ src/week1/BabyLionBonus1_Main.java
@@ -0,0 +1,54 @@
+package week1;
+
+import java.util.Scanner;
+
+public class BabyLionBonus1_Main {
+
+    static void main() {
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("🦁저장할 아기사자 수를 5 이상 입력해주세요.");
+            int size = scanner.nextInt();
+
+            while (size < 5) {
+                System.out.println("❗️[오류] 5 이상 입력해주세요.");
+                System.out.println("🦁저장할 아기사자 수를 5 이상 입력해주세요.");
+                size = scanner.nextInt();
+            }
+
+            String[] name = new String[size];
+
+            System.out.println("✏️아기사자 이름을 입력해주세요.");
+            scanner.nextLine();
+
+            for (int i = 0 ; i < size; i++) {
+                name[i] = scanner.nextLine();
+            }
+
+            System.out.println("📋아기사자 명단을 최종적으로 출력합니다.");
+            for (int i = 0 ; i < size; i++) {
+                System.out.println("🦁" + (i + 1) + ". " + name[i]);
+            }
+
+            while (true) { //exit, restart 기능 추가
+                System.out.println("===================");
+                System.out.println("⛔️프로그램을 종료하려면 'exit'를 입력하세요");
+                System.out.println("🔁계속 아기사자를 등록하려면 'restart'를 입력하세요");
+
+                String input = scanner.next();
+
+                if (input.equals("restart")) {
+                    break;
+                } else if (input.equals("exit")) {
+                    System.out.println("👋아기 사자 명단 관리 프로그램을 종료합니다.");
+                    return;
+                } else{
+                    System.out.println("⚠️잘못된 입력 값입니다. 다시 입력해주세요");
+                }
+            }
+        }
+    }
+}
+
