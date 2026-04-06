a5ccf8fb2323faebf3f5545ff4a3511a5b76daff
장선호 <tmmjang@naver.com>
2026-04-06 15:33:47 +0900

    기능 추가2 test

--- src/test/TestMain.java
+++ src/test/TestMain.java
@@ -5,5 +5,6 @@ public class TestMain {
     static void main() {
         System.out.println("Hello, world!");
         System.out.println("기능1 추가");
+        System.out.println("기능2 추가");
     }
 }
