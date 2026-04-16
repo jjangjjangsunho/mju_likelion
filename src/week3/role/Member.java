6f75c86b82d269f3d380be65effe12094300eb24
장선호 <tmmjang@naver.com>
2026-04-17 00:33:54 +0900

    feat : 공통 속성(이름, 전공, 기수, 파트)을 관리하는 추상 클래스 생성

---
+++ src/week3/role/Member.java
@@ -0,0 +1,41 @@
+package week3.role;
+
+import week3.policy.Policy;
+
+abstract public class Member implements Policy {
+    private String name;
+    private String major;
+    private int cohort;
+    private String part;
+
+    public Member(String name, String major, int cohort, String part) {
+        this.name = name;
+        this.major = major;
+        this.cohort = cohort;
+        this.part = part;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public int getCohort() {
+        return cohort;
+    }
+
+    public String getPart() {
+        return part;
+    }
+
+    public abstract Policy getPolicy();
+
+    public abstract String getDetails();
+
+    public boolean isSubmittable(){
+        return getPolicy().isSubmittable();
+    }
+}
