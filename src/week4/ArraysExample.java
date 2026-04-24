447006b8fe3d417266cccde0dc70097d07b5b1d7
장선호 <tmmjang@naver.com>
2026-04-24 17:23:28 +0900

    배열

---
+++ src/week4/ArraysExample.java
@@ -0,0 +1,27 @@
+package week4;
+
+public class ArraysExample {
+
+    static void main() {
+        // 배열(Arrays)
+
+        int[] price = {10000, 9000, 40000, 7000};
+        String[] mbti = {"INFP", "ENFP", "ISTJ", "ESTP"};
+
+        System.out.println(mbti.length);
+
+        for (int i = 0 ;i < mbti.length; i++){
+            System.out.println(mbti[i]);
+        }
+
+
+        //System.out.println(price[0]);
+        //System.out.println(mbti[0]);
+
+        //price[1] = 8000;
+        //System.out.println(price[1]);
+
+        //System.out.println(price);
+
+    }
+}
